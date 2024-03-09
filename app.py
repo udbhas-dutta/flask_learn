5073268c57514bd02b61934084fb26f3b4ab1b7d
Udbhas Dutta <udbhasdutta@gmail.com>
2024-03-09 11:51:10 +0530

    added database and some sample data

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///taskapp.db"
 db = SQLAlchemy(app)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+            return f"{self.sno} - {self.title}"
 
-@app.route("/products")
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        title= request.form['title']
+        desc= request.form['desc']
+        
+        task = Todo(title=title, desc=desc)
+        db.session.add(task)
+        db.session.commit()
+    allTasks = Todo.query.all()
+    return render_template('index.html', allTasks=allTasks)
+
+@app.route("/show")
 def products():
+    allTasks = Todo.query.all()
+    print(allTasks)
     return "<h1>This is the products page</h1>"
 
 if __name__ == "__main__":
